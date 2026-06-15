830e150f02b8279995acdf946e090dc41824565c
EmersonOrellana <ot20006@ues.edu.sv>
2026-06-15 17:24:53 -0600

    Configracion para Render

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ function getConexion() {
     $pass = "f5b6f02f16d969e3d0beeb8e28e8662b0eb96e71";
 
     try {
-        $pdo = new PDO("mysql:host=1p1ljt.h.filess.io;port=3306;dbname=$db;charset=utf8mb4", $user, $pass);
+        $pdo = new PDO("mysql:host=lp1ljt.h.filess.io;port=3306;dbname=$db;charset=utf8mb4", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
