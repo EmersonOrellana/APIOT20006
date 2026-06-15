0548c519e262ef7bc086c5dd83540612573178b5
EmersonOrellana <ot20006@ues.edu.sv>
2026-06-15 16:20:47 -0600

    Añadiendo Dockerfile para Render

--- conexion.php
+++ conexion.php
@@ -1,11 +1,15 @@
 <?php
 function getConexion() {
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbname = "control_doc&hosp";
-    $conexion = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $conexion;
+    $host = "1p1ljt.h.filess.io";
+    $db = "control_doc_hosp_scenedugso";
+    $user = "control_doc_hosp_scenedugso";
+    $pass = "f5b6f02f16d969e3d0beeb8e28e8662b0eb96e71";
+
+    try {
+        $pdo = new PDO("mysql:host=$host;port=3306;dbname=$db;charset=utf8mb4", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Error de conexión: " . $e->getMessage());
+    }
 }
-?>
